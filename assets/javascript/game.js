4227f1f27c1d3a4e984550ac12fde6a761e18e2a
unknown <jkue3000@gmail.com>
2017-11-21 00:33:41 -0500

    Some HTML and little JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// Array of letters to be referenced from
+var letterChoice ['a', 'b', 'c', 'd', 'e',
+				  'f', 'g', 'h', 'i', 'j',
+				  'k', 'l', 'm', 'n', 'o',
+				  'p', 'q', 'r', 's', 't',
+				  'u', 'v', 'w', 'x', 'y', 
+				  'z'];
+
+// Word Bank array
+var wordBank = ['fishing', 'ocean', 'mermaid', 'surf', 'pescado'];
+
+// Score keeper from user input
+var userWins = 0;
+var userLoses = 0;
+var userGuess= 9;
+
+// Start of the game
+document.onkeyup = function(event) {
+
+	var letterGuessed = event.key
+
+}
+
+
+
+
+// if (user.onKeyUp == words[])
